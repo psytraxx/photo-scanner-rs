63151b409bd3acd78a568d4e73ac5b3cd6a9b1b4
eric <psytraxx@gmail.com>
2024-10-27 12:00:01 +0100

    remove complexity - do not stream files - add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,39 @@
 use anyhow::{anyhow, Result};
-use futures::future::join_all;
-use futures::stream::{Stream, StreamExt};
+use futures::stream::{iter, StreamExt};
+use indicatif::{ProgressBar, ProgressStyle};
 use photo_scanner_rust::domain::ports::Chat;
 use photo_scanner_rust::outbound::image_provider::resize_and_base64encode_image;
 use photo_scanner_rust::outbound::openai::OpenAI;
 use photo_scanner_rust::outbound::xmp::{
     extract_persons, get_xmp_description, write_xmp_description,
 };
+use std::fs::read_dir;
 use std::path::{Path, PathBuf};
-use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Instant;
-use tokio::spawn;
-use tokio::sync::Semaphore;
 use tracing::{error, info, warn};
 
 // Maximum number of concurrent tasks for ollama multimodal API
 const MAX_CONCURRENT_TASKS: usize = 2;
 
-// Function to list files in a directory and its subdirectories
-fn list_files(directory: PathBuf) -> Pin<Box<dyn Stream<Item = Result<PathBuf>> + Send>> {
-    let initial_read_dir = tokio::fs::read_dir(directory);
+/// Function to list files in a directory and its subdirectories.
+fn list_files<P: AsRef<Path>>(path: P) -> Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+    for entry in read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
 
-    // Create an initial stream that will be used for recursion
-    let stream = async_stream::try_stream! {
-        let mut read_dir = initial_read_dir.await?;
-        while let Some(entry) = read_dir.next_entry().await? {
-            let path = entry.path();
-            if path.is_file() {
-                // Yield the file path
-                yield path;
-            } else if path.is_dir() {
-                // Recursively list files in the subdirectory
-                let sub_stream = list_files(path);
-                // Flatten the subdirectory stream into the current stream
-                for await sub_path in sub_stream {
-                    yield sub_path?;
-                }
-            }
+        if path.is_dir() {
+            // Recursively traverse subdirectories
+            files.extend(list_files(path)?);
+        } else {
+            files.push(path);
         }
-    };
-
-    Box::pin(stream)
+    }
+    Ok(files)
 }
 
-// Function to check if the path has a valid JPEG extension
+/// Function to check if the path has a valid JPEG extension.
 fn is_jpeg(path: &Path) -> bool {
     match path.extension().and_then(|ext| ext.to_str()) {
         Some(ext) => matches!(ext.to_ascii_lowercase().as_str(), "jpg" | "jpeg"),
@@ -52,8 +41,10 @@ fn is_jpeg(path: &Path) -> bool {
     }
 }
 
+/// Main entry point.
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Set up tracing for logging.
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_ansi(true)
@@ -61,142 +52,125 @@ async fn main() -> Result<()> {
         .without_time()
         .init();
 
-    let args: Vec<String> = std::env::args().collect();
+    // Initialize the OpenAI chat model.
+    let chat = Arc::new(OpenAI::new());
 
+    // Get the folder path from command line arguments.
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         return Err(anyhow!("Please provide a path to the folder."));
     }
-
     let root_path = PathBuf::from(&args[1]);
 
-    // Traverse the files and print them
-    let mut files_stream = list_files(root_path);
+    // Traverse the files and process them with limited concurrency.
+    let files_list = list_files(root_path)?;
 
-    let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_TASKS));
-    let mut tasks = Vec::new();
+    // Create a progress bar with the total length of the vector.
+    let progress_bar = Arc::new(ProgressBar::new(files_list.len() as u64));
+    progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")?,
+    );
 
-    while let Some(file_result) = files_stream.next().await {
-        let path = match file_result {
-            Ok(path) => path,
-            Err(e) => {
-                error!("Error: {}", e);
-                continue;
-            }
-        };
-
-        // no need to create a task if it can be skipped
-        if can_be_skipped(&path) {
-            continue;
-        }
-
-        let semaphore = Arc::clone(&semaphore);
-        let chat: OpenAI = OpenAI::new();
-
-        let task = spawn(async move {
-            let permit = match semaphore.acquire().await {
-                Ok(permit) => permit,
-                Err(e) => {
-                    error!("Failed to acquire semaphore for {}: {}", path.display(), e);
+    iter(files_list)
+        .for_each_concurrent(MAX_CONCURRENT_TASKS, |path| {
+            let progress_bar = Arc::clone(&progress_bar);
+            let chat = Arc::clone(&chat);
+            async move {
+                progress_bar.inc(1);
+                // Skip files that do not need processing.
+                if can_be_skipped(&path) {
                     return;
                 }
-            };
 
-            let start_time = Instant::now();
+                let start_time = Instant::now();
 
-            let persons = match extract_persons(&path) {
-                Ok(persons) => persons,
-                Err(e) => {
-                    warn!("Error extracting persons from {}: {}", &path.display(), &e);
-                    Vec::new()
-                }
-            };
+                // Extract persons from the image, handling any errors.
+                let persons = match extract_persons(&path) {
+                    Ok(persons) => persons,
+                    Err(e) => {
+                        warn!("Error extracting persons from {}: {}", path.display(), e);
+                        Vec::new() // Default to an empty list if extraction fails.
+                    }
+                };
 
-            let image_base64 = resize_and_base64encode_image(&path).unwrap();
+                // Resize and encode the image as base64.
+                let image_base64 = match resize_and_base64encode_image(&path) {
+                    Ok(encoded) => encoded,
+                    Err(e) => {
+                        error!("Error encoding image {}: {}", path.display(), e);
+                        return;
+                    }
+                };
 
-            let folder_name: Option<String> = path
-                .parent()
-                .and_then(|p| p.file_name()?.to_str().map(|s| s.to_string()));
+                // Optionally get the folder name for additional context.
+                let folder_name: Option<String> = path
+                    .parent()
+                    .and_then(|p| p.file_name()?.to_str().map(str::to_string));
 
-            let description = match chat.get_chat(&image_base64, &persons, &folder_name).await {
-                Ok(description) => description,
-                Err(e) => {
+                // Generate a description using the chat model.
+                let description = match chat.get_chat(&image_base64, &persons, &folder_name).await {
+                    Ok(desc) => desc,
+                    Err(e) => {
+                        error!("Error generating description for {}: {}", path.display(), e);
+                        return;
+                    }
+                };
+
+                /* if let Err(e) = chat.get_embedding(&description).await {
+                    error!("Error getting embedding for {}: {}", &path.display(), e);
+                } */
+
+                if let Err(e) = write_xmp_description(&description, &path) {
                     error!(
-                        "Error extracting image description from {}: {}",
-                        &path.display(),
-                        &e
+                        "Error storing XMP description for {}: {}",
+                        path.display(),
+                        e
                     );
-                    drop(permit);
-                    return;
                 }
-            };
 
-            /* if let Err(e) = chat.get_embedding(&description).await {
-                error!("Error getting embedding for {}: {}", &path.display(), e);
-            } */
-
-            if let Err(e) = write_xmp_description(&description, &path) {
-                error!(
-                    "Error storing XMP description for {}: {}",
+                // Log the time taken and other details.
+                let duration = Instant::now() - start_time;
+                info!(
+                    "Generated \"{}\" for \"{}\", Time taken: {:.2} seconds, Persons: {:?}",
+                    description,
                     path.display(),
-                    e
+                    duration.as_secs_f64(),
+                    persons
                 );
             }
+        })
+        .await;
 
-            let duration = Instant::now() - start_time;
-            info!(
-                "Generated \"{}\" for \"{}\", Time taken: {:.2} seconds, Persons: {:?}",
-                &description,
-                &path.display(),
-                duration.as_secs_f64(),
-                &persons
-            );
-
-            drop(permit);
-        });
-
-        tasks.push(task)
-    }
-
-    // Wait for all the tasks to complete before exiting the method.
-    let results = join_all(tasks).await;
-
-    for result in results {
-        if let Err(e) = result {
-            error!("Task failed: {:?}", e);
-        }
-    }
+    progress_bar.finish_with_message("All items have been processed.");
 
     Ok(())
 }
 
-// Function to check if the file needs to be processed
+/// Function to check if the file can be skipped.
 fn can_be_skipped(path: &Path) -> bool {
-    // do create task for non-jpeg files
     if !is_jpeg(path) {
-        return true;
+        return true; // Skip non-JPEG files.
     }
 
-    // Check if the EXIF description already exists and skip the file if it does.
+    // Skip files that already have an XMP description.
     match get_xmp_description(path) {
         Ok(Some(description)) => {
             info!(
                 "Description \"{}\" exists for \"{}\"",
-                &description,
-                &path.display()
+                description,
+                path.display()
             );
-            return true;
+            true
         }
-        Ok(None) => {
-            // Continue code execution if there is no description
-        }
+        Ok(None) => false, // No description present, proceed with processing.
         Err(e) => {
             error!(
                 "Error getting XMP description for {}: {}",
-                &path.display(),
-                &e
+                path.display(),
+                e
             );
-            return true;
+            true // Skip processing if there's an error retrieving the description.
         }
     }
-    false
 }
